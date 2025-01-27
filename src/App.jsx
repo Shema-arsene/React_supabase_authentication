f6718edbd023c5914689daf2f6dafe49a2af37b6
Shema-arsene <aniyonkurushema@gmail.com>
2025-01-28 01:52:32 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-center font-bold uppercase">Hello World!</h1>
+      <h1 className="text-3xl text-center font-bold uppercase my-40">
+        React Authentication with Supabase
+      </h1>
     </>
   )
 }
